9dbb781ee16d88c3c67cd88c974fc9190df8dbd3
imsagar07 <kalesaket7@gmail.com>
2024-03-30 16:22:49 +0530

    first commit

---
+++ src/app/tab2/tab2.module.ts
@@ -0,0 +1,18 @@
+import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab2Page } from './tab2.page';
+
+import { Tab2PageRoutingModule } from './tab2-routing.module';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    Tab2PageRoutingModule
+  ],
+  declarations: [Tab2Page],schemas:[CUSTOM_ELEMENTS_SCHEMA]
+})
+export class Tab2PageModule {}
